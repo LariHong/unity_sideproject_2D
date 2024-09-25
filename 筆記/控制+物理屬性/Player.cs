cb9197939f2750038fd2ea3f2449daa5b6498a03
Lari <gccbs05591gw2@gmail.com>
2024-09-25 23:10:10 +0800

    Revert "步驟三 了解SerializeField GetComponent Rigidbody2D.velocity"
    This reverts commit d02049e0eea365b0a31e58779e80af45a290f259.

--- 筆記/控制+物理屬性/Player.cs
+++ 筆記/控制+物理屬性/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,20 +6,16 @@ public class Player : MonoBehaviour
 {
     public float xInput;
     public float yInput;
-    public float moveSpeed;
-    public float jumpForce;
-    [SerializeField]
-    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb= GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //正常的 get key
+        //���`�� get key
         //if(Input.GetKeyDown(KeyCode.Space))
         //{
         //    Debug.Log("jump");
@@ -33,36 +29,18 @@ public class Player : MonoBehaviour
         //    Debug.Log("up");
         //}
 
-        //GetAxis 會有平滑值 接近 -1~0~1
+        //�ϥ� GetButton �n�h unity���� edit > project settings > input manager > axes �M��
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("up");
+        }
+
+        //GetAxis �|�����ƭ� ���� -1~0~1
         //xInput = Input.GetAxis("Horizontal");
         //yInput = Input.GetAxis("Vertical");
 
-        //GetAxis 只有值 -1 0 1
+        //GetAxis �u���� -1 0 1
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
-
-        ////使用 GetButton 要去 unity中的 edit > project settings > input manager > axes 尋找
-        //if (Input.GetButton("Jump"))
-        //{
-        //    Debug.Log("up");
-        //}
-
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-        // 如果有水平输入，讓它滚动
-        if (xInput != 0)
-        {
-            float torque = -xInput * 2; // 使用負號讓滚动方向正确
-            rb.AddTorque(torque);
-        }
-        else
-        {
-            // 如果没有输入，停止角速度，立即停止滚动
-            rb.angularVelocity = 0;
-        }
     }
 }
